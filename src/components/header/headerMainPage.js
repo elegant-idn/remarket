52eaec1f64c3b45ee0bc1b7ec6971de4a7a24534
elegant-idn <curtisconley34@gmail.com>
2023-11-23 03:21:09 -0800

    ecommerce

---
+++ src/components/header/headerMainPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import HeaderTop from './headerTop'
+import HeaderMobile from '../mobile/header/headerMobile';
+
+const HeaderMainPage = ({ params, isJobPage }) => {
+        return (
+                <header >
+                        <HeaderTop params={params} isJobPage={isJobPage}/>
+                        {(window.isMobile && !window.isTablet) && <HeaderMobile menu={true} title='<img alt="Logo" src="/images/design/logo_all_pages.svg"/>'/>}
+                </header>
+        )
+}
+
+export default HeaderMainPage
+
